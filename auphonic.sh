9fb6c932fd8b256f8e3d0bbb815f06be9f33f5db
heathdutton <heathdutton@gmail.com>
2024-06-11 14:23:54 -0400

    2024-06-11 18:23:54

--- auphonic.sh
+++ auphonic.sh
@@ -18,19 +18,27 @@ if [ -f "$NEW_FILE" ]; then
     exit 0
 fi
 
+UUID=""
 if [ -f "$AU_FILE" ]; then
   UUID=$(cat "$AU_FILE")
   echo "Already uploaded. UUID is $UUID"
 else
   echo "Processing $FILE with title $TITLE"
-  JSON=$(curl -s -X POST https://auphonic.com/api/simple/productions.json \
+  # Keep running while UUID is empty
+  while [ -z "$UUID" ]; do
+    JSON=$(curl -s -X POST https://auphonic.com/api/simple/productions.json \
       -u "$AUPHONIC_USERNAME:$AUPHONIC_PASSWORD" \
       -F "preset=oFbHxxvDT2Wrg9CjV5xgBQ" \
       -F "title=$TITLE" \
       -F "input_file=@$FILE" \
       -F "action=start")
 
-  UUID=$(echo "$JSON" | jq -r '.data.uuid')
+    UUID=$(echo "$JSON" | jq -r '.data.uuid')
+    if [ -z "$UUID" ]; then
+      echo "Error processing file. Retrying in 30 seconds"
+      sleep 30
+    fi
+  done
   echo "UUID is $UUID"
 fi
 
@@ -38,14 +46,15 @@ AU_FILE=$(echo "$FILE" | sed 's/\.[^.]*$/.au/')
 echo "$UUID" > "$AU_FILE"
 
 while true; do
-  JSON=$(curl -X GET "https://auphonic.com/api/production/$UUID.json" \
-  -u "$AUPHONIC_USERNAME:$AUPHONIC_PASSWORD")
+  echo -n "."
+  JSON=$(curl -s -X GET "https://auphonic.com/api/production/$UUID.json" \
+    -u "$AUPHONIC_USERNAME:$AUPHONIC_PASSWORD")
   # echo "$JSON"
   STATUS=$(echo "$JSON" | jq -r '.data.status_string')
   if [ "$STATUS" == "Done" ]; then
     break
   fi
-  sleep 30
+  sleep 15
 done
 
 DOWNLOAD_URL=$(echo "$JSON" | jq -r '.data.output_files[0].download_url')
