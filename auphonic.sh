39ff118b7165484d20d9f8074da1d077a485bd70
heathdutton <heathdutton@gmail.com>
2025-01-12 00:25:16 -0500

    2025-01-12 05:25:16

--- auphonic.sh
+++ auphonic.sh
@@ -8,22 +8,33 @@ TITLE="$2"
 NEW_FILE="$3"
 AU_FILE=$(echo "$FILE" | sed 's/\.[^.]*$/.au/')
 
-if [ ! -f "$FILE" ]; then
-    echo "File $FILE does not exist"
-    exit 1
+if [ -f "$NEW_FILE" ]; then
+    echo "Auphonic skipping: $FILE already processed"
+    exit 0
 fi
 
-if [ -f "$NEW_FILE" ]; then
-    echo "File $FILE already processed"
+# Find another mp3 file already based on the same video
+VIDEO_ID=$(echo "$NEW_FILE" | gsed 's/.*\/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}-\([a-zA-Z0-9_-]*\)\.mp3/\1/')
+EXISTING_FILE=$(find . -type f -name "*$VIDEO_ID.mp3" | grep -v "$NEW_FILE" | head -n 1)
+if [ -f "$EXISTING_FILE" ]; then
+    echo "Auphonic skipping: $FILE already processed (previous file $EXISTING_FILE)"
+    cp "$EXISTING_FILE" "$NEW_FILE"
+    EXISTING_AU_FILE=$(echo "$EXISTING_FILE" | sed 's/\.[^.]*$/.au/')
+    cp "$EXISTING_AU_FILE" "$AU_FILE"
     exit 0
+fi
+
+if [ ! -f "$FILE" ]; then
+    echo "Auphonic skipping: $FILE does not exist"
+    exit 1
 fi
 
 UUID=""
 if [ -f "$AU_FILE" ]; then
   UUID=$(cat "$AU_FILE")
-  echo "Already uploaded. UUID is $UUID"
+  echo -n "Auphonic already received UUID: $UUID"
 else
-  echo "Processing $FILE with title $TITLE"
+  echo -n "Auphonic is processing: $FILE with title $TITLE"
   # Keep running while UUID is empty
   while [ -z "$UUID" ]; do
     JSON=$(curl -s -X POST https://auphonic.com/api/simple/productions.json \
@@ -39,7 +50,7 @@ else
       sleep 30
     fi
   done
-  echo "UUID is $UUID"
+  # echo "UUID is $UUID"
 fi
 
 AU_FILE=$(echo "$FILE" | sed 's/\.[^.]*$/.au/')
@@ -54,9 +65,9 @@ while true; do
   if [ "$STATUS" == "Done" ]; then
     break
   fi
-  sleep 15
+  sleep 10
 done
 
 DOWNLOAD_URL=$(echo "$JSON" | jq -r '.data.output_files[0].download_url')
-echo "Downloading $DOWNLOAD_URL to $NEW_FILE"
+echo "Auphonic complete, downloading to $NEW_FILE"
 curl -o "$NEW_FILE" "$DOWNLOAD_URL" -u "$AUPHONIC_USERNAME:$AUPHONIC_PASSWORD" > /dev/null 2>&1
